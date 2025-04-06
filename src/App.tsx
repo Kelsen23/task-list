872b81e175a64b7df4e506dbb96c3ebc46cbf1a9
Kelsen23 <nikolozigoreziani@gmail.com>
2025-04-06 12:19:16 +0400

    Set up MainArea and Sidebar components in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import MainArea from "./components/MainArea"
+import Sidebar from "./components/Sidebar"
 
 const App = () => {
   return (
     <div>
-      
+      <Sidebar />
+
+      <div className="flex-1 p-6">
+        <MainArea />
+      </div>
     </div>
   )
 }
---
+++ src/components/MainArea.tsx
@@ -0,0 +1,10 @@
+
+const MainArea = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default MainArea
