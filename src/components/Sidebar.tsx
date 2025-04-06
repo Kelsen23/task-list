8d138b95ca9bd0433869614fef977224ad8d0acd
Kelsen23 <nikolozigoreziani@gmail.com>
2025-04-06 14:01:24 +0400

    Render lists and workspaces in Sidebar componet

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -10,19 +10,46 @@ const Sidebar = () => {
         <div className="p-4">
           <h3 className="text-lg font-semibold flex items-center">Lists</h3>
 
-          <button onClick={openListModal} className="flex justify-center items-center mt-[1rem] cursor-pointer">
+          <button
+            onClick={openListModal}
+            className="flex justify-center items-center mt-[1rem] cursor-pointer"
+          >
             <FaPlus className="mr-2" /> List
           </button>
+
+          <ul>
+            {list.map((list, index) => (
+              <li
+                key={index}
+                className="p-2 hover:bg-[#ccc] rounded-lg cursor-pointer flex items-center mt-2"
+              >
+                <span className="mr-2">{list.emoji}</span> {list.name}
+              </li>
+            ))}
+          </ul>
         </div>
 
         <div className="p-4">
-          <h3 className="text-lg font-semibold flex items-center">  
+          <h3 className="text-lg font-semibold flex items-center">
             Workspaces
           </h3>
 
-            <button onClick={openWorkspaceModal} className="flex justify-center items-center mt-[1rem] cursor-pointer">
-              <FaPlus className="mr-2" /> Worspaces
-            </button>
+          <button
+            onClick={openWorkspaceModal}
+            className="flex justify-center items-center mt-[1rem] cursor-pointer"
+          >
+            <FaPlus className="mr-2" /> Worspaces
+          </button>
+          <ul>
+            {workspace.map((workspace, index) => (
+              <li
+                key={index}
+                className="p-2 hover:bg-[#ccc] rounded-lg cursor-pointer flex items-center mt-2"
+              >
+                <span className="mr-2">{workspace.emoji}</span> {workspace.name}
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </div>
